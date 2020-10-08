2faee3ac38f8fa7e2e3412568720ea383a6a1300
Ridla Fadilah <ridla.fadilah@gmail.com>
2020-10-09 01:50:17 +0700

    dongkap

--- @dongkap/do-common/src/lib/input/text/do-input-text.component.ts
+++ @dongkap/do-common/src/lib/input/text/do-input-text.component.ts
@@ -17,10 +17,42 @@ export class DoInputTextComponent extends DoValueAccessor<string> {
     @Input() min: number = 0;
     @Input() max: number = 999;
     @Input() step: number = 1;
+    @Input() capslock: boolean = false;
     @Input() type: 'text' | 'password' | 'number' = 'text';
 
     constructor(@Optional() @Self() ngControl: NgControl,
       @Inject(LOCALE_ID) public locale: string) {
       super(ngControl, locale);
     }
+
+    onKeyUp(event: KeyboardEvent) {
+      if (this.capslock) {
+        this.value = this.value.toUpperCase();
+      }
+    }
+
+    onKeyDown(event: KeyboardEvent) {
+      if (this.type === 'number') {
+        if (this.value.length >= this.maxLength) {
+          if (
+            ['DELETE', 'BACKSPACE', 'TAB', 'ESCAPE', 'ENTER'].indexOf(event.key.toUpperCase()) !== -1 ||
+            (event.key.toUpperCase() === 'A' && event.ctrlKey === true) || // Allow: Ctrl+A
+            (event.key.toUpperCase() === 'C' && event.ctrlKey === true) || // Allow: Ctrl+C
+            (event.key.toUpperCase() === 'X' && event.ctrlKey === true) || // Allow: Ctrl+X
+            (event.key.toUpperCase() === 'A' && event.metaKey === true) || // Cmd+A (Mac)
+            (event.key.toUpperCase() === 'C' && event.metaKey === true) || // Cmd+C (Mac)
+            (event.key.toUpperCase() === 'X' && event.metaKey === true) || // Cmd+X (Mac)
+            (event.key.toUpperCase() === 'END') ||
+            (event.key.toUpperCase() === 'HOME') ||
+            (event.key.toUpperCase() === 'LEFT ARROW') ||
+            (event.key.toUpperCase() === 'RIGHT ARROW')) {
+              return true;
+          }
+          return false;
+        }
+        return true;
+      }
+      return true;
+    }
+
 }
